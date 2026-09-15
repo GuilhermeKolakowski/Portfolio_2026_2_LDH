01738bb4849e9ce387563ed28694b4b333174d10
Guilherme Kolakowski <guilhermekl99@gmail.com>
2026-09-15 10:05:44 -0300

    Adiciona pasta Exemplos (teste_mux) e assignment defaults faltantes

---
+++ Exemplos/Testes e exemplos/teste_mux/mux2.v
@@ -0,0 +1,18 @@
+//comportamental
+
+//mux padrao inicial
+module mux2(
+	input a,
+	input b,
+	input sel,
+	output reg y
+
+
+);
+	always @(*) begin
+		if(sel) y = b;
+		else	  y = a;
+	end
+
+
+endmodule
